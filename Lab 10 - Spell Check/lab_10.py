2f85b522f62ee9aeaf454d3176577ab546dcd740
jeffrey.harbour <jeffrey.harbour@my.simpson.edu>
2020-10-28 11:34:09 -0500

    Finished binary search

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -1,3 +1,6 @@
+# Importing libraries
+import re
+
 # Making constants
 DICTIONARY_FILE_NAME: str = "dictionary.txt"
 KEY_WORD = ""
@@ -5,6 +8,10 @@ IS_CASE_SENSITIVE: bool = False
 
 
 # Defining functions
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+
 def get_list_from_file(file_name):
     file = open(file_name)
     file_list: list = []
@@ -18,7 +25,7 @@ def get_list_from_file(file_name):
     return file_list
 
 
-def search_in_list(key, searching_list: list, case_insensitive: bool = False):
+def linear_search_through_list(key, searching_list: list, case_insensitive: bool = False):
     if case_insensitive:
         for current_list_index in range(len(searching_list)):
             if searching_list[current_list_index].lower() == key.lower():
@@ -30,11 +37,36 @@ def search_in_list(key, searching_list: list, case_insensitive: bool = False):
     return False, None
 
 
+def binary_search_through_list(key, searching_list: list, case_insensitive: bool = False):
+    lower_bound: int = 0
+    upper_bound: int = len(searching_list) - 1
+    if case_insensitive:
+        while lower_bound <= upper_bound:
+            current_list_index: int = (lower_bound + upper_bound) // 2
+            if searching_list[current_list_index].lower() < key.lower():
+                lower_bound = current_list_index + 1
+            elif searching_list[current_list_index].lower() > key.lower():
+                upper_bound = current_list_index - 1
+            else:
+                return True, current_list_index
+    else:
+        while lower_bound <= upper_bound:
+            current_list_index: int = (lower_bound + upper_bound) // 2
+            if searching_list[current_list_index] < key:
+                lower_bound = current_list_index + 1
+            elif searching_list[current_list_index] > key:
+                upper_bound = current_list_index - 1
+            else:
+                return True, current_list_index
+    return False, None
+
+
 # Defining main function
 def main():
     # Making variables
     dictionary_list: list = get_list_from_file(DICTIONARY_FILE_NAME)
-    is_in_list, list_number = search_in_list(KEY_WORD, dictionary_list, not IS_CASE_SENSITIVE)
+    is_in_list, list_number = binary_search_through_list(KEY_WORD, dictionary_list, not IS_CASE_SENSITIVE)
+
     if is_in_list:
         print(f"{KEY_WORD} is a word.")
         print(f"It is the {list_number + 1}th word in the dictionary.")
